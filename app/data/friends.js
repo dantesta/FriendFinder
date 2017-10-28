abb6f644be12b522f9bb17e5d869115c878980db
Dan Testa <dantesta@Dans-MacBook-Pro-3.local>
2017-10-27 22:29:43 -0400

    friend list and npm

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendsList = [{
+    name: 'Dan Testa',
+    picture: '',
+    scores: []
+}, {
+    name: 'Matt Ruggiero',
+    picture: 'https://media.licdn.com/media/AAEAAQAAAAAAAAqYAAAAJGMxYmNhZjI2LWM2MGMtNDZkZS1hYTBiLWEwNWM0NTBiYjY3YQ.jpg',
+    scores: []
+}, {
+    name: 'Jack Ross',
+    picture: 'https://media.licdn.com/media/AAEAAQAAAAAAAAgOAAAAJGIxY2ZkMzI1LTAxOTctNDFjOS1hY2VkLTIzZThhOGUyY2NmYg.jpg',
+    scores: []
+}, {
+    name: 'David Van Doren',
+    picture: 'https://s3-media1.fl.yelpcdn.com/bphoto/MdaftnsAtD0Xbxlx5vciAA/o.jpg',
+    scores: []
+}];
+
+module.exports = friendsList;
