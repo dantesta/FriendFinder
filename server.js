c298397aee0e729b3e8f03f91f9b5a8b14599896
Dan Testa <dantesta@Dans-MacBook-Pro-3.local>
2017-10-27 22:41:07 -0400

    serverjs done, needs api routes and survey html

--- server.js
+++ server.js
@@ -4,3 +4,23 @@ var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+var apiRoutes = require('./app/routing/apiRoutes.js');
+var htmlRoutes = require('./app/routing/htmlRoutes.js');
+
+
+app.use('/', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
